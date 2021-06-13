47e8aff1802fdb4701266f3dadf87bcb39f52551
Vitthal Saruk <48468620+vitthalsaruk@users.noreply.github.com>
2021-06-13 11:29:17 +0530

    Update ExecuteAUTNew.java

--- src/main/java/com/employee/pract/ExecuteAUTNew.java
+++ src/main/java/com/employee/pract/ExecuteAUTNew.java
@@ -6,10 +6,9 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 public class ExecuteAUTNew {
     public static void main(String... args) throws UnirestException {
         //Unirest.setTimeouts(0, 0);
-        HttpResponse<String> response = Unirest.post("https://jdf-pega-devl.deere.com/prweb/PRRestService/BRMUnitTest/Rule-Test-Unit-Case/ExecuteBRMTests?TestSuiteID=" +
-                "DEERE-USCA-BRM-WORK!TS_US_E2EVALIDATION")
+        HttpResponse<String> response = Unirest.post("")
                 .header("Content-Type", "text/xml")
-                .header("Authorization", "Basic VVNBVVRJRDpBOCFiVnRSYQ==")
+                .header("Authorization", "Basic VVNBVVRJRDpBOCFiVnRSYQ")
                 .asString();
 
         System.out.println(response.getBody().toString());
